7bf0d8f7eb36f49c513d6b53e9d5241168965b62
Scott Bollinger <kfalconspb@bollingerusa.com>
2018-02-24 16:08:41 -0800

    redo

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -1,13 +1,10 @@
-// Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
-});
+// GoogleAnalyticsScript
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-48318522-2', 'auto');
+  ga('send', 'pageview');
 
 // Navigation Scripts to Show Header on Scroll-Up
 jQuery(document).ready(function($) {
